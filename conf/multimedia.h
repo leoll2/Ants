a263c4a2de6a6d2072aced13466e3921eb5e0ad8
Leonardo <leonardo.lai@live.com>
2018-11-25 00:13:17 +0100

    graphics parameters

---
+++ conf/multimedia.h
@@ -0,0 +1,16 @@
+#ifndef CONF_MULTIMEDIA_H
+#define CONF_MULTIMEDIA_H
+
+/* COLORS */
+#define COLOR_RED       makecol(0xFF,0x00,0x00)
+#define COLOR_GREEN     makecol(0x00,0xFF,0x00)
+#define COLOR_BLUE      makecol(0x00,0x00,0xFF)
+
+
+#define WCET_GRAPHICS		60
+#define PRD_GRAPHICS		100
+#define DL_GRAPHICS			80
+#define PRIO_GRAPHICS		30
+
+
+#endif
