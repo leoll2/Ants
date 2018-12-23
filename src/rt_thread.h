123547b315a024df7fbb87337dbc6d34d5c7e0f8
Leonardo Lai <33199121+leoll2@users.noreply.github.com>
2018-12-23 11:24:48 +0100

    fixed tabs

--- src/rt_thread.h
+++ src/rt_thread.h
@@ -4,31 +4,29 @@
 #include <pthread.h>
 #include <stdbool.h>
 
-
 #define MAX_THREADS		256
-
 
 typedef struct task_par {
 
-	bool in_use;		// structure is free or in use
-	pthread_mutex_t mtx;// lock on the structure
-	pthread_t tid;		// thread id
-	void *(*behaviour)(void *);	// behaviour code
-	void *data;			// behaviour-specific data
-	int period; 		// period (ms)
-	int deadline; 		// relative deadline (ms)
-	int priority; 		// priority [0,99]
-	int dl_missed;		// no. of deadline misses
-	bool stopped;		// thread was ordered to stop
-	struct timespec at; // next activ. time
-	struct timespec dl; // abs. deadline
+	bool 		in_use;		// structure is free or in use
+	pthread_mutex_t mtx; 		// lock on the structure
+	pthread_t 	tid;		// thread id
+	void *		(*behaviour)(void *);	// behaviour code
+	void *		data;		// behaviour-specific data
+	int 		period; 	// period (ms)
+	int 		deadline; 	// relative deadline (ms)
+	int 		priority; 	// priority [0,99]
+	int 		dl_missed;	// no. of deadline misses
+	bool 		stopped;	// thread was ordered to stop
+	struct timespec at; 		// next activ. time
+	struct timespec dl; 		// abs. deadline
 } task_par;
 
 
 void init_rt_thread_manager(void);
 
 int start_thread(
-		void *(*func)(void *), // routine code
+		void *(*func)(void *),  // routine code
 		void *args,		// routine args
 		int policy,		// scheduling policy
 		int prd,		// period (ms)
