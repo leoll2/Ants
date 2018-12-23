d7fde722a7a7b0860bd2741e1931c285fc15a624
Leonardo Lai <33199121+leoll2@users.noreply.github.com>
2018-12-23 11:21:18 +0100

    fixed tabs

--- src/ant.h
+++ src/ant.h
@@ -16,23 +16,23 @@ extern const float TWO_PI;
 typedef enum behaviour {EXPLORING, EXPLOITING, EATING, RESTING} behaviour;
 
 typedef struct position {
-	int 			x;			// [pixel] x coordinate
-	int 			y;			// [pixel] y coordinate
+	int 			x;		// [pixel] x coordinate
+	int 			y;		// [pixel] y coordinate
 	float 			angle;		// [radians] angle in [0, 2*pi)
 } position;
 
 
 typedef struct ant {
 	bool 			alive;		// is ant allocated? is the data meaningful?
-	unsigned int 	tid;		// thread identifier
+	unsigned int 		tid;		// thread identifier
 	position 		pos;		// ant position
 	fragrance		interest;	// looking for food or home?
 	behaviour		behaviour;	// following a trail or exploring?
 	float			excitement;	// intensity of the pheromone to be released
 	float 			audacity;	// propensity for explorative decisions
 	bool			diverted;	// has the ant recently diverted from its path to explore?
-	unsigned int 	expl_desire;// ticks left before the ant stops exploring
-	pthread_mutex_t mtx;		// mutex to protect the struct
+	unsigned int 		expl_desire;	// ticks left before the ant stops exploring
+	pthread_mutex_t 	mtx;		// mutex to protect the struct
 } ant;
 
 
